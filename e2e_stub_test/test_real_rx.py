66b1d7d2fb6b49c0bea6c63be1322e852808563f
Khal Buyo <mynameisrainer@gmail.com>
2019-03-05 01:24:30 +0800

    data rate computation

--- e2e_stub_test/test_real_rx.py
+++ e2e_stub_test/test_real_rx.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import socket
+import time
 
 ADDR = "127.0.0.1"
 RX = 7888
@@ -7,6 +8,23 @@ rx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 rx.bind((ADDR, RX))
 
+started = False
+transferred = 0
+packets = 0
 while True:
     data, addr = rx.recvfrom(256) # buffer size is 1024 bytes
+    if (False == started):
+        start = time.time()
+        started = True
+    transferred += len(data)
+    packets += 1
     print "received: ", data
+    if ("end"==data):
+        elapsed = time.time() - start
+        started = False
+        print "Elapsed: ", elapsed
+        print "transferred: ", transferred
+        print "Data Rate: " + str((transferred/elapsed)*8) + " bps"
+        print "Packet Rate: " + str(packets/elapsed) + " packets/s"
+        transferred = 0
+        packets = 0
--- e2e_stub_test/test_real_tx.py
+++ e2e_stub_test/test_real_tx.py
@@ -7,9 +7,10 @@ LORA_RX = 7899
 sx1278_tx = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 i=0
-while True:
-    data = "TEST MESSAGE " + str(i)
+while (i<1000):
+    data = "TEST MESSAGE " + str(i) + "-----------------------------------------------"
     sx1278_tx.sendto(data, (ADDR, LORA_RX))
     print "sent: ", data
-    time.sleep(0.015)
+    time.sleep(0.03333)
     i += 1
+sx1278_tx.sendto("end", (ADDR, LORA_RX))
