0038d273a3b472c81badbc3f92b1a910e8ce2fe2
khalbuyo <pbuyo@apsiwifi.com>
2020-12-22 12:29:58 +0800

    remove old

--- prepare_external.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git submodule update --init --recursive
-
-pushd . && cd PiGpioHwApi && mkdir -p build && cd build && ../configure.py && make pigpiohwapi.a && popd
-pushd . && cd Logless && mkdir -p build && cd build && ../configure.py && make logless.a && make spawner && popd
